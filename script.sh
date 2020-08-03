919bdea32fbbc303b71cc6aef172a508c36fa13a
Veera Sivarajan <sveera.2001@gmail.com>
2020-08-03 17:08:33 +0530

    working as intended

--- script.sh
+++ script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 function kural() {
-  num=1
-  tag='# ' 
+  randNum=`shuf -i 1-1330 -n 1`
+  num=$((${1:-${randNum}}-1))
+  oneTag='# '
+  echo ${oneTag}$((${num}+1)) > kural.md
+  echo ${randNum}
+  tag='## ' 
   line=`cat thirukkural.json | jq --argjson index "$num" '.kural[$index] | .Line1 + " " + .Line2' | sed -e 's/^"//' -e 's/"$//'` 
   fullLine=${tag}${line}
   dash="- "
@@ -10,9 +14,10 @@ function kural() {
   trans2=$(cat thirukkural.json | jq --argjson index "$num" '.kural[$index] | .sp' | sed -e 's/^"//' -e 's/"$//')
   trans3=$(cat thirukkural.json | jq --argjson index "$num" '.kural[$index] | .mv' | sed -e 's/^"//' -e 's/"$//')
   trans4=$(cat thirukkural.json | jq --argjson index "$num" '.kural[$index] | .Translation' | sed -e 's/^"//' -e 's/"$//')
-  echo ${fullLine} > kural.md
+  echo ${fullLine} >> kural.md
   echo ${dash}${trans1} >> kural.md
   echo ${dash}${trans2} >> kural.md
   echo ${dash}${trans3} >> kural.md
   echo ${dash}${trans4} >> kural.md
+  (okular kural.md &)
 }
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-num=10
-line=`cat thirukkural.json | jq --argjson index "$num" '.kural[$index] | .Line1 + " " + .Line2' | sed -e 's/^"//' -e 's/"$//'`
-echo ${line}
+randNum=`shuf -i 0-100 -n 1`
+num=${1:-${randNum}}
+echo ${num}
+echo $((${num} + 1)) 
