6e8b244a53c56012398feb062fb839aa50b195b1
Veera Sivarajan <sveera.2001@gmail.com>
2020-08-03 17:13:51 +0530

    execute from any directory

--- script.sh
+++ script.sh
@@ -4,20 +4,20 @@ function kural() {
   randNum=`shuf -i 1-1330 -n 1`
   num=$((${1:-${randNum}}-1))
   oneTag='# '
-  echo ${oneTag}$((${num}+1)) > kural.md
+  echo ${oneTag}$((${num}+1)) > /home/veera/Projects/.ThirukkuralAPI/kural.md
   echo ${randNum}
   tag='## ' 
-  line=`cat thirukkural.json | jq --argjson index "$num" '.kural[$index] | .Line1 + " " + .Line2' | sed -e 's/^"//' -e 's/"$//'` 
+  line=`cat /home/veera/Projects/.ThirukkuralAPI/thirukkural.json | jq --argjson index "$num" '.kural[$index] | .Line1 + " " + .Line2' | sed -e 's/^"//' -e 's/"$//'` 
   fullLine=${tag}${line}
   dash="- "
-  trans1=$(cat thirukkural.json | jq --argjson index "$num" '.kural[$index] | .mk' | sed -e 's/^"//' -e 's/"$//')
-  trans2=$(cat thirukkural.json | jq --argjson index "$num" '.kural[$index] | .sp' | sed -e 's/^"//' -e 's/"$//')
-  trans3=$(cat thirukkural.json | jq --argjson index "$num" '.kural[$index] | .mv' | sed -e 's/^"//' -e 's/"$//')
-  trans4=$(cat thirukkural.json | jq --argjson index "$num" '.kural[$index] | .Translation' | sed -e 's/^"//' -e 's/"$//')
-  echo ${fullLine} >> kural.md
-  echo ${dash}${trans1} >> kural.md
-  echo ${dash}${trans2} >> kural.md
-  echo ${dash}${trans3} >> kural.md
-  echo ${dash}${trans4} >> kural.md
-  (okular kural.md &)
+  trans1=$(cat /home/veera/Projects/.ThirukkuralAPI/thirukkural.json | jq --argjson index "$num" '.kural[$index] | .mk' | sed -e 's/^"//' -e 's/"$//')
+  trans2=$(cat /home/veera/Projects/.ThirukkuralAPI/thirukkural.json | jq --argjson index "$num" '.kural[$index] | .sp' | sed -e 's/^"//' -e 's/"$//')
+  trans3=$(cat /home/veera/Projects/.ThirukkuralAPI/thirukkural.json | jq --argjson index "$num" '.kural[$index] | .mv' | sed -e 's/^"//' -e 's/"$//')
+  trans4=$(cat /home/veera/Projects/.ThirukkuralAPI/thirukkural.json | jq --argjson index "$num" '.kural[$index] | .Translation' | sed -e 's/^"//' -e 's/"$//')
+  echo ${fullLine} >> /home/veera/Projects/.ThirukkuralAPI/kural.md
+  echo ${dash}${trans1} >> /home/veera/Projects/.ThirukkuralAPI/kural.md
+  echo ${dash}${trans2} >> /home/veera/Projects/.ThirukkuralAPI/kural.md
+  echo ${dash}${trans3} >> /home/veera/Projects/.ThirukkuralAPI/kural.md
+  echo ${dash}${trans4} >> /home/veera/Projects/.ThirukkuralAPI/kural.md
+  (okular /home/veera/Projects/.ThirukkuralAPI/kural.md &)
 }
